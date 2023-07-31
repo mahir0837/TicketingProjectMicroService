27c2ffff38f54e154d0be912d84e3ce5b7c96a3b
owner <mahirsarac89@gmail.com>
2023-07-31 14:54:46 -0400

    Gateway added

--- gateway-service/src/main/java/com/cydeo/GatewayServerApplication.java
+++ gateway-service/src/main/java/com/cydeo/GatewayServerApplication.java
@@ -2,14 +2,14 @@ package com.cydeo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaServer
-public class DiscoveryServiceApplication {
+@EnableDiscoveryClient
+public class GatewayServerApplication {
 
     public static void main(String[] args) {
 
-        SpringApplication.run(DiscoveryServiceApplication.class,args);
+        SpringApplication.run(GatewayServerApplication.class,args);
     }
 }
